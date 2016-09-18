0f05bdd8d0b0f5734d32653f69fa0620b38125f5
ash804 <lway804@163.com>
2016-09-18 14:00:46 +0800

    Add excel version scoreAnalyticsV1.0

---
+++ core/src/com/edu/score/test/TestAnalyzeChemistryScore.java
@@ -0,0 +1,11 @@
+package com.edu.score.test;
+import org.testng.annotations.Test;
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class TestAnalyzeChemistryScore {
+    @Test
+    public void test(){
+        System.out.println("HI");
+    }
+}
